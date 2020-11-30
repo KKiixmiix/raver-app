bcd11e8c73fb7d74ed55c9ccee9a358b5db41f58
ad9369 <60271088+ad9369@users.noreply.github.com>
2020-11-29 19:04:52 -0500

    Add files via upload

--- users/manage.php
+++ users/manage.php
@@ -2,67 +2,64 @@
 require_once '../_common.php';
 login();
 
-/*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
-if (!empty($_POST['userid'] ?? '')) {
-  $userid = sanitize('userid');
-  [$type, $userid] = explode('-', $userid) + [null, null];
-  if (!(in_array($type, ['u', 'd']) && is_numeric($userid))) {
-    main();
-    exit('<h2>Something is not right</h2>');
-  }
+# Get type of action and entity id:
+[$type, $userid] = getTypeAndId(sanitize('userid'), $entity = 'user');
 
-  require_once('../DBconfig.php');
-
-  # Perform DELETE and exit
-  if ($type == 'd') {
-    main();
-    $query = "DELETE FROM users WHERE userid = ?";
-    $stmt = mysqli_prepare($dbc, $query);
-    mysqli_stmt_bind_param($stmt, "i", $userid);
-    if (!mysqli_stmt_execute($stmt)) {
-      $echo = '<h2>We were unable to delete the user at this time.</h2>';
-    } else {
-      $echo = '<h2>The user was successfully deleted.</h2>';
-    }
-    mysqli_close($dbc);
-    exit($echo);
-  }
-
-  # Prepare UPDATE
-  $query = "SELECT invitedby, last_name, first_name, email, phone, password FROM users WHERE userid=?";
-  $stmt = mysqli_prepare($dbc, $query);
-  mysqli_stmt_bind_param($stmt, "i", $userid);
-  if (!mysqli_stmt_execute($stmt)) {
-    mysqli_close($dbc);
-    main();
-    exit('<h2>Oh no! Something went wrong!</h2>' . mysqli_error($dbc));
-  }
-  $result = mysqli_stmt_get_result($stmt);
-  $users = mysqli_fetch_assoc($result);
-  extract($users);
-  mysqli_close($dbc);
+# Perform DELETE and exit with corresponding message:
+if ($type == 'd') {
+  delete($entity, 'users', 'userid', $userid);
 }
-else {
-  main();
-  exit('<h2>You have reached this page in error</h2>');
+
+# Prepare UPDATE
+if ($type == 'u') {
+  $query = 'SELECT invitedby, last_name, first_name, email, phone, password FROM users WHERE userid=?';
+  extract(manage($entity, $query, 'i', $userid));
 }
+
+# Get a list of all users for invitors list:
+$query = 'SELECT userid uid, first_name fnm, last_name lnm FROM users WHERE userid<>? ORDER BY userid';
+$invitors = sql($query, 'i', $userid);
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <title>Raver App</title>
     <meta charset="utf-8">
+    <style>
+      table th{text-align:right;vertical-align:top}
+      table td{min-width:20em}
+      table td select{width:100%}
+      table td input{width:calc(100% - 8px)}
+    </style>
   </head>
   <body>
     <?php main(); ?>
     <h2>Edit User profile (ID <?=$userid?>):</h2>
-    <form action="<?=$url?>/users/update.php" method="post">
-      <h3>Raver patron:  <input type="text" name="first_name" value="<?=$first_name?>"></h3>
-      <h3>Last name:  <input type="text" name="last_name" value="<?=$last_name?>"></h3>
-      <h3>Contact email: <input type="text" name="email" value="<?=$email?>"></h3>
-      <h3>Contact number: <input type="text" name="phone" value="<?=$phone?>"></h3>
-      <h3>Password: <input type="password" name="password" value=""></h3>
-      <h3>Invited by User ID: <input type="text" name="invitedby" value="<?=$invitedby?>"></h3>
+    <form action="<?=url('users/update.php')?>" method="post">
+      <table border=1 cellpadding=5 style="border-collapse: collapse; margin-bottom: 1ex;">
+        <tr><th>First Name:</th>
+            <td><input type="text" name="first_name" value="<?=$first_name?>"></td></tr>
+        <tr><th>Last Name:</th>
+            <td><input type="text" name="last_name" value="<?=$last_name?>"></td></tr>
+        <tr><th>Contact Email:</th>
+            <td><input type="text" name="email" value="<?=$email?>"></td></tr>
+        <tr><th>Phone Number:</th>
+            <td><input type="text" name="phone" value="<?=$phone?>"></td></tr>
+        <tr><th>Invited By User:</th>
+            <td>
+              <select name="invitedby">
+                <option value="">&nbsp; &nbsp;<[nobody]></option>
+<?php foreach ($invitors as $invitor): extract($invitor); ?>
+                <option value="<?=$uid?>"<?=selected($invitedby==$uid)?>><?=fullName($lnm, $fnm)?></option>
+<?php endforeach; ?>
+              </select>
+            </td></tr>
+<?php if ($loggedIn == $userid): ?>
+        <tr><th>Password:</th>
+            <td><input type="password" name="password" value="">
+                <br>Leave blank to keep current password.</td></tr>
+<?php endif; ?>
+      </table>
       <input type="hidden" name="userid" value="<?=$userid?>">
       <input type="submit" value="Update">
     </form>
