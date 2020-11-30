813aaa70d13d6d9f81deadef6aa0f51efec52555
ad9369 <60271088+ad9369@users.noreply.github.com>
2020-11-29 19:04:14 -0500

    Add files via upload

--- venues/list.php
+++ venues/list.php
@@ -2,25 +2,8 @@
 require_once '../_common.php';
 login();
 
-/*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
-require_once('../DBconfig.php');
-
-$query = "SELECT venueid, name, address, contact FROM venues";
-$stmt = mysqli_prepare($dbc, $query);
-
-// mysqli_stmt_bind_param($stmt);
-
-if(!mysqli_stmt_execute($stmt)) {
-  echo "<h2>Oh no! Something went wrong!</h2>".mysqli_error($dbc);
-  mysqli_close($dbc);
-  exit;
-}
-$result = mysqli_stmt_get_result($stmt);
-# Songs found
-if (mysqli_num_rows($result)) {
-  $venues = mysqli_fetch_all($result, MYSQLI_ASSOC);
-}
-mysqli_close($dbc);
+# Venues found:
+$venues = sql('SELECT venueid, name, address, contact FROM venues');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,21 +13,19 @@ mysqli_close($dbc);
   </head>
   <body>
     <?php main(); ?>
-    <h2>Venue Location</h2>
-    <form action="<?=$url?>/venues/manage.php" method="post">
-      <table border=1 cellpadding=5 style="border-collapse: collapse;">
+    <h2>Venues</h2>
+    <form action="<?=url('venues/manage.php')?>" method="post">
+      <table border=1 cellpadding=5 style="border-collapse: collapse; margin-bottom: 1ex;">
         <tr>
-          <th>ID</th>
-          <th>Name</th>
+          <th>Venue Name</th>
           <th>Address</th>
           <th>Contact</th>
           <th>Edit</th>
           <th>Delete</th>
         </tr>
-<?php foreach ($venues??[] as $venue): extract($venue); ?>
+<?php foreach ($venues as $venue): extract($venue); ?>
         <tr>
-          <th><?=$venueid?></th>
-          <td><?=$name   ?></td>
+          <td><?=$name?></td>
           <td><?=$address?></td>
           <td><?=$contact?></td>
           <th><input type="radio" name="venueid" value="u-<?=$venueid?>"></th>
@@ -52,10 +33,8 @@ mysqli_close($dbc);
         </tr>
 <?php endforeach; ?>
       </table>
-      <input type="submit" value="Edit/Delete">
+      <input type="submit" value="Edit / Delete">
+      <button formaction="<?=url('venues/add.php')?>">Add new venue</button>
     </form>
-    <ul>
-      <li><a href="<?=$url?>/venues/add.php"><b>Add venue</b></a></li>
-    </ul>
   </body>
 </html>
--- venues/update.php
+++ venues/update.php
@@ -2,38 +2,13 @@
 require_once '../_common.php';
 login();
 
-/*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
-if (!empty($_POST['venueid'] ?? '')) {
-  $venueid = sanitize('venueid');
-  $name  = sanitize('name');
-  $address = sanitize('address');
-  $contact = sanitize('contact');
-  require_once('../DBconfig.php');
-  $message = "$venueid/$name/$address/$contact";
+# Get venue id, quit if none was given:
+quit_unless($venueid = sanitize('venueid'), 'No venue ID was passed for processing.');
+# Get the rest of the passed values:
+$name    = sanitize('name');
+$address = sanitize('address');
+$contact = sanitize('contact');
 
-  # UPDATE
-  $query = "UPDATE venues SET name = ?, address = ?, contact = ? WHERE venueid = ?";
-  $stmt = mysqli_prepare($dbc, $query);
-  mysqli_stmt_bind_param($stmt, "sssi", $name, $address, $contact, $venueid);
-  if (!mysqli_stmt_execute($stmt)) {
-    $message = "We were unable to update the venue at this time.";
-  } else {
-    $message = "The venue location \"$name\" was successfully updated.";
-  }
-  $message .= mysqli_error($dbc);
-} else {
-  $message = "You have reached this page in error";
-}
-mysqli_close($dbc);
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Raver App</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <?php main(); ?>
-    <h2><?=$message?></h2>
-  </body>
-</html>
+# UPDATE
+$query = 'UPDATE venues SET name=?, address=?, contact=? WHERE venueid=?';
+update('venue', $query, 'sssi', $venueid, $name, $address, $contact, $venueid);
