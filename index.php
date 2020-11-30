186ca0e60306f00d407c7c5404d87b0149e467a1
ad9369 <60271088+ad9369@users.noreply.github.com>
2020-11-29 19:07:09 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ require_once '_common.php';
     <h3>Links to queries:</h3>
     <ul>
 <?php if ($loggedIn): ?>
-      <li><a href="<?=$url?>/songs/add.php">Add a song!</a></li>
-      <li><a href="<?=$url?>/songs/list.php">List my music</a></li>
+      <li><a href="<?=url('songs/add.php')?>">Add a song!</a></li>
+      <li><a href="<?=url('songs/list.php')?>">List my music</a></li>
 <?php else: ?>
-      <li><a href="<?=$url?>/raver_login.php">Welcome Login Page</a></li>
-      <li><a href="<?=$url?>/users/add_newuser.php">Sign up</a></li>
+      <li><a href="<?=url('raver_login.php')?>">Welcome Login Page</a></li>
+      <li><a href="<?=url('users/add_newuser.php')?>">Sign up</a></li>
 <?php endif; ?>
     </ul>
   </body>
