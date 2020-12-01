7562d86217cacf1f1c57f0aee0d1c74b05179dd3
ad9369 <60271088+ad9369@users.noreply.github.com>
2020-11-30 21:29:12 -0500

    Add files via upload

--- venues/manage.php
+++ venues/manage.php
@@ -2,29 +2,49 @@
 require_once '../_common.php';
 login();
 
-# Get type of action and entity id:
-#[$type, $venueid] = getTypeAndId(sanitize('venueid'), $entity = 'venue');
-[$type, $venueid] = explode("-",sanitize('venueid'));
-# Perform DELETE and exit with corresponding message:
-#echo $type;
-#echo $venueid;
-if ($type == 'd') {
-  $query = "DELETE FROM venues WHERE venueid= $venueid";
-  sql($query);
-  #delete($entity, 'venues', 'venueid', $venueid);
-  #url('venues/list.php');
-  #$url = "Refresh; Location: ";
-  #$url .= url('venues/list.php');
-  #header($url);
-  #  exit;
+/*This code assumes user input is valid and correct only for demo purposes - it does NOT validate form data.*/
+if (!empty($_POST['venueid'] ?? '')) {
+  $venueid = sanitize('venueid');
+  [$type, $venueid] = explode('-', $venueid) + [null, null];
+  if (!(in_array($type, ['u', 'd']) && is_numeric($venueid))) {
+    main();
+    exit('<h2>Something is not right</h2>');
+  }
+
+  require_once('../DBconfig.php');
+
+  # Perform DELETE and exit
+  if ($type == 'd') {
+    main();
+    $query = "DELETE FROM venues WHERE venueid = ?";
+    $stmt = mysqli_prepare($dbc, $query);
+    mysqli_stmt_bind_param($stmt, "i", $venueid);
+    if (!mysqli_stmt_execute($stmt)) {
+      $echo = '<h2>We were unable to delete the venue at this time.</h2>';
+    } else {
+      $echo = '<h2>The venue was successfully deleted.</h2>';
+    }
+    mysqli_close($dbc);
+    exit($echo);
+  }
+
+  # Prepare UPDATE
+  $query = "SELECT name, address, contact FROM venues WHERE venueid=?";
+  $stmt = mysqli_prepare($dbc, $query);
+  mysqli_stmt_bind_param($stmt, "i", $venueid);
+  if (!mysqli_stmt_execute($stmt)) {
+    mysqli_close($dbc);
+    main();
+    exit('<h2>Oh no! Something went wrong!</h2>' . mysqli_error($dbc));
+  }
+  $result = mysqli_stmt_get_result($stmt);
+  $venues = mysqli_fetch_assoc($result);
+  extract($venues);
+  mysqli_close($dbc);
 }
-
-# Prepare UPDATE
-if ($type == 'u') {
-  $query = "SELECT name, address, contact FROM venues WHERE venueid=$venueid";
-  $venueRow = sql($query);
-
-  #extract(manage($entity, $query, 'i', $venueid));
+else {
+  main();
+  exit('<h2>You have reached this page in error</h2>');
 }
 ?>
 <!DOCTYPE html>
@@ -32,25 +52,14 @@ if ($type == 'u') {
   <head>
     <title>Raver App</title>
     <meta charset="utf-8">
-    <style>
-      table th{text-align:right;vertical-align:top}
-      table td{min-width:20em}
-      table td input{width:calc(100% - 8px)}
-    </style>
   </head>
   <body>
     <?php main(); ?>
     <h2>Edit your venue (ID <?=$venueid?>):</h2>
-    <?php extract($venueRow);?>
-    <form action="<?=url('venues/update.php')?>" method="post">
-      <table border=1 cellpadding=5 style="border-collapse: collapse; margin-bottom: 1ex;">
-        <tr><th>Name:</th>
-            <td><input type="text" name="name" value="<?=$name?>"></td></tr>
-        <tr><th>Address:</th>
-            <td><input type="text" name="address" value="<?=$address?>"></td></tr>
-        <tr><th>Contact:</th>
-            <td><input type="text" name="contact" value="<?=$contact?>"></td></tr>
-      </table>
+    <form action="<?=$url?>/venue/update.php" method="post">
+      <h3>Name:  <input type="text" name="name" value="<?=$name?>"></h3>
+      <h3>Address: <input type="text" name="address" value="<?=$address?>"></h3>
+      <h3>Contact: <input type="text" name="contact" value="<?=$contact?>"></h3>
       <input type="hidden" name="venueid" value="<?=$venueid?>">
       <input type="submit" value="Update">
     </form>
